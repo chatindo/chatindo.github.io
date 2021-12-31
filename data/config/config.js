5425ebf63ca8fdeeb0e1d2a0f974b2bac38213af
lesst than renzen <69730462+chatindo@users.noreply.github.com>
2021-12-31 21:00:27 +0800

    Update config.js

--- data/config/config.js
+++ data/config/config.js
@@ -1,10 +1,9 @@
 const firebaseConfig = {
-    apiKey: "GANTI DENGAN PROJECT KALIAN",
-    authDomain: "GANTI DENGAN PROJECT KALIAN",
-    databaseURL: "GANTI DENGAN PROJECT KALIAN",
-    projectId: "GANTI DENGAN PROJECT KALIAN",
-    storageBucket: "GANTI DENGAN PROJECT KALIAN",
-    messagingSenderId: "GANTI DENGAN PROJECT KALIAN",
-    appId: "GANTI DENGAN PROJECT KALIAN"
+    apiKey: "AIzaSyDyuE7A1Y28vmA6tOt_lERpN_XiZuZFYCE",
+    authDomain: "pesan-25f69.firebaseapp.com",
+    projectId: "pesan-25f69",
+    storageBucket: "pesan-25f69.appspot.com",
+    messagingSenderId: "1081814949841",
+    appId: "1:1081814949841:web:5badc8e93d2ebaf9ca0af0"
 };
 const app = firebase.initializeApp(firebaseConfig);
