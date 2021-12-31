d9638f0cf28c31a19551d1d04ecc718872b844a5
lesst than renzen <69730462+chatindo@users.noreply.github.com>
2021-12-31 22:03:44 +0800

    Update config.js

--- data/config/config.js
+++ data/config/config.js
@@ -1,10 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDbhhwufXVRqXRyg_Ug3PgeXkefw7ZxPxA",
-  authDomain: "hello-72656.firebaseapp.com",
-  projectId: "hello-72656",
-  storageBucket: "hello-72656.appspot.com",
-  messagingSenderId: "84544439032",
-  appId: "1:84544439032:web:eff76f74b878800d76b10d",
-  measurementId: "G-2LL7DZ3MH1"
+  apiKey: "AIzaSyA797Tj9XngjWlcVsnTByDuGNlof7gumUw",
+  authDomain: "awesome-ab272.firebaseapp.com",
+  projectId: "awesome-ab272",
+  storageBucket: "awesome-ab272.appspot.com",
+  messagingSenderId: "802994559790",
+  appId: "1:802994559790:web:453a4e2fb299b7fdf2b8bf",
+  measurementId: "G-MM0GK7ZPR0"
 };
 const app = firebase.initializeApp(firebaseConfig);
