5541467d4d9ed73629e328bce46b4f1a2288a572
lesst than renzen <69730462+chatindo@users.noreply.github.com>
2021-12-31 21:09:01 +0800

    Update config.js

--- data/config/config.js
+++ data/config/config.js
@@ -1,9 +1,3 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDyuE7A1Y28vmA6tOt_lERpN_XiZuZFYCE",
-    authDomain: "pesan-25f69.firebaseapp.com",
-    projectId: "pesan-25f69",
-    storageBucket: "pesan-25f69.appspot.com",
-    messagingSenderId: "1081814949841",
-    appId: "1:1081814949841:web:5badc8e93d2ebaf9ca0af0"
 };
 const app = firebase.initializeApp(firebaseConfig);
